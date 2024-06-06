9dc680520c11e97b754ec56c3b80a4f57d3b3e62
onyeka-hub <onyekagodonu@yahoo.com>
2024-06-06 18:46:43 +0100

    babel configuration

--- app/frontend/src/App.test.js
+++ app/frontend/src/App.test.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders product management header', () => {
+test('renders product management header', async () => {
   render(<App />);
-  const headerElement = screen.getByText(/Product Management/i);
+  const headerElement = await screen.findByText(/Product Management/i);
   expect(headerElement).toBeInTheDocument();
 });
 
-test('renders product form', () => {
+test('renders product form', async () => {
   render(<App />);
-  const nameLabel = screen.getByLabelText(/Product Name/i);
-  const priceLabel = screen.getByLabelText(/Product Price/i);
+  const nameLabel = await screen.findByLabelText(/Product Name/i);
+  const priceLabel = await screen.findByLabelText(/Product Price/i);
   expect(nameLabel).toBeInTheDocument();
   expect(priceLabel).toBeInTheDocument();
 });
 
-test('renders product list', () => {
+test('renders product list', async () => {
   render(<App />);
-  const listHeader = screen.getByText(/Product List/i);
+  const listHeader = await screen.findByText(/Product List/i);
   expect(listHeader).toBeInTheDocument();
 });
